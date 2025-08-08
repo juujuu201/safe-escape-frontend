e8aafa57717d986e83caccbe2e8b1e17d4565f55
juhyun2.lee <juhyun2.lee@gmail.com>
2025-08-09 01:36:15 +0900

    :art: 파일 분리
    - 컴포넌트 파일 분리

---
+++ src/view/CongestionView.js
@@ -0,0 +1,217 @@
+import {useEffect, useState} from "react";
+import {useModel} from "../controller/UseModel.js";
+import Constants from "../common/Constants.js";
+import Resources from "../common/Resources.js";
+import Define from "../common/Define.js";
+import Util from "../common/Utils.js";
+import {appModel, MenuButtonModel} from "../model/AppModel.js";
+import {SEMessageBar, SETextButton} from "../widgets/Widgets.js";
+import AppController from "../controller/AppController.js";
+
+const _viewNames = Constants.VIEW_NAMES,
+    _menuNames = Constants.MENU_NAMES,
+    _congestionButtonValues = Constants.CONGESTION_BUTTON_VALUES,
+    _statusType = Constants.STATUS_TYPE,
+    _mapMoveEnableOptions = Define.MAP_MOVE_ENABLE_OPTIONS,
+    _defaultButtonGroup = [
+        _congestionButtonValues.SET_CONGESTION,
+        _congestionButtonValues.WALKING_DISTANCE,
+        _congestionButtonValues.FIND_SHELTER,
+        _congestionButtonValues.RECOMMEND_EXIT
+    ],
+    _menuButtonGroup = {
+        [`${_statusType.NONE}`]: _defaultButtonGroup,
+        [`${_statusType.CONGESTION_SETTING}`]: [
+            _congestionButtonValues.CANCEL_CONGESTION_SETTING,
+            _congestionButtonValues.SET_EXIT,
+            _congestionButtonValues.STOP_CONGESTION_SETTING
+        ],
+        [`${_statusType.EXIT_SETTING}`]: [
+            _congestionButtonValues.RESET_EXIT,
+            _congestionButtonValues.FINISH_EXIT,
+            _congestionButtonValues.STOP_CONGESTION_SETTING
+        ],
+        [`${_statusType.CONGESTION_SELECTED}`]: _defaultButtonGroup,
+        [`${_statusType.EXIT_SELECTED}`]: _defaultButtonGroup
+    };
+
+export const CongestionSideBarView = () => {
+    const status = useModel(appModel, "status"),
+        messageValue = useModel(appModel, "messageValue"),
+        [visibleModels, setVisibleModels] = useState([]),
+        isMessageBarVisible = (status === Constants.STATUS_TYPE.CONGESTION_SETTING || status === Constants.STATUS_TYPE.EXIT_SETTING);
+
+    useEffect(() => {
+        const buttonModels = [];
+
+        for (const [key, value] of Object.entries(_congestionButtonValues)) {
+            const model = new MenuButtonModel({
+                desc: Resources[key],
+                value
+            });
+
+            buttonModels.push(model);
+        }
+
+        appModel.setValue("menuButtonModels", buttonModels);
+    }, []);
+
+    useEffect(() => {
+        const menuButtonGroup = _menuButtonGroup[status];
+
+        if (status && menuButtonGroup) {
+            const nextVisibleModels = [];
+
+            for (const model of appModel.menuButtonModels) {
+                const value = model.value,
+                    isVisible = (menuButtonGroup.indexOf(value) !== -1);
+                let isDisable = true,
+                    messageValue;
+
+                if (model.isVisible !== isVisible) {
+                    model.setValue("isVisible", isVisible);
+                }
+
+                if (isVisible) {
+                    switch(status) {
+                        case _statusType.CONGESTION_SETTING:
+                            if (value === _congestionButtonValues.CANCEL_CONGESTION_SETTING) {
+                                isDisable = false;
+                            }
+                            messageValue = Resources.SETTING_CONGESTION_AREA;
+                            break;
+                        case _statusType.EXIT_SETTING:
+                            if (value === _congestionButtonValues.RESET_EXIT) {
+                                isDisable = false;
+                            }
+                            messageValue = Resources.SETTING_EXIT;
+                            break;
+                        case _statusType.NONE:
+                            if (value === _congestionButtonValues.SET_CONGESTION) {
+                                isDisable = false;
+                            }
+                            break;
+                    }
+
+                    if (value === _congestionButtonValues.STOP_CONGESTION_SETTING) {
+                        isDisable = false;
+                    }
+
+                    if (model.isDisabled !== isDisable) {
+                        model.setValue("isDisabled", isDisable);
+                    }
+
+                    if (messageValue) {
+                        appModel.setValue("messageValue", messageValue);
+                    }
+
+                    nextVisibleModels.push(model);
+                }
+            }
+
+            setVisibleModels(nextVisibleModels);
+        }
+    }, [status]);
+
+    return (
+        <div className={Constants.CONGESTION_MENU_AREA_CLASS}>
+            <SEMessageBar desc={messageValue} isVisible={isMessageBarVisible}/>
+            <div className={`${_menuNames.CONGESTION} ${_viewNames.SIDE_BAR}`}>
+                {visibleModels.map(model =>
+                    <CongestionMenuButton key={model.value} model={model}/>)}
+            </div>
+        </div>
+    );
+};
+
+const CongestionMenuButton = (props) => {
+    const {model} = props,
+        {value, desc} = model,
+        map = useModel(appModel, "map"),
+        isVisible = useModel(model, "isVisible"),
+        isDisabled = useModel(model, "isDisabled");
+
+    function _onClick(e) {
+        let isEnableMap = false,
+            status;
+
+        switch (value) {
+            // 혼잡 지역 설정하기
+            case _congestionButtonValues.SET_CONGESTION:
+                status = _statusType.CONGESTION_SETTING;
+
+                // 혼잡 지역을 설정하는 동안 지도를 움직이지 못하도록 설정한다.
+                if (map) {
+                    const mapOptions = Object.fromEntries(
+                        Object.entries(_mapMoveEnableOptions).map(([key, value]) => [key, !value])
+                    );
+
+                    if (!Util.isEmptyObject(mapOptions)) {
+                        map.setOptions(mapOptions);
+                    }
+                }
+
+                // 마커 툴팁이 열려있었다면 닫는다.
+                AppController.closeMapTooltip();
+                break;
+
+            // 혼잡 지역 초기화
+            case _congestionButtonValues.CANCEL_CONGESTION_SETTING:
+                AppController.cancelCongestionSetting(map);
+                break;
+
+            // 도보 거리 계산
+            case _congestionButtonValues.WALKING_DISTANCE:
+                break;
+
+            // 가까운 대피소 찾기
+            case _congestionButtonValues.FIND_SHELTER:
+                break;
+
+            // 비상구 추천
+            case _congestionButtonValues.RECOMMEND_EXIT:
+                break;
+
+            // 비상구 설정하기
+            case _congestionButtonValues.SET_EXIT:
+                status = _statusType.EXIT_SETTING;
+                break;
+
+            // 비상구 초기화
+            case _congestionButtonValues.RESET_EXIT:
+                AppController.cancelExitSetting(map);
+                break;
+
+            // 비상구 설정 완료
+            case _congestionButtonValues.FINISH_EXIT:
+                isEnableMap = true;
+                AppController.cancelExitSetting(map);
+                appModel.setValue("status", Constants.STATUS_TYPE.NONE);
+                break;
+
+            // 혼잡 지역 설정 취소
+            case _congestionButtonValues.STOP_CONGESTION_SETTING:
+                isEnableMap = true;
+                AppController.cancelCongestionSetting(map, true);
+                appModel.setValue("status", Constants.STATUS_TYPE.NONE);
+                break;
+        }
+
+        if (status) {
+            appModel.setValue("status", status);
+        }
+
+        // 지도를 다시 움직일 수 있도록 설정한다.
+        if (isEnableMap) {
+            map.setOptions(Define.MAP_MOVE_ENABLE_OPTIONS);
+        }
+    }
+
+    if (!isVisible) {
+        return null;
+    }
+
+    return (
+        <SETextButton desc={desc} isDisabled={isDisabled} isVisible={isVisible} onClick={_onClick} value={value}/>
+    );
+};
---
+++ src/view/HomeView.js
@@ -0,0 +1,110 @@
+import {useEffect, useState} from "react";
+import {useModel} from "../controller/UseModel.js";
+import Constants from "../common/Constants.js";
+import Resources from "../common/Resources.js";
+import Util from "../common/Utils.js";
+import {appModel, CongestionModel, MarkerModel} from "../model/AppModel.js";
+import {SEImage, SEText, SETextButton} from "../widgets/Widgets.js";
+
+const _naverMap = window.naver,
+    _viewNames = Constants.VIEW_NAMES,
+    _menuNames = Constants.MENU_NAMES;
+
+export const HomeSideBarView = () => {
+    const shelterList = [       /** TODO: 테스트용 */
+            {
+                id: "1",
+                name: "인수동 자치회관",
+                address: "서울특별시 강북구 인수봉로 255",
+                latitude: 37.635451,
+                longitude: 127.019194
+            },
+            {
+                id: "2",
+                name: "성균관대학교 자연과학캠퍼스",
+                address: "경기 수원시 장안구 서부로 2066",
+                latitude: 37.293929,
+                longitude: 126.974348
+            }
+        ],
+        congestionList = [
+            {
+                latitude: 37.646713,
+                longitude: 127.024274,
+                level: Constants.CROWDED_LEVEL.FREE
+            },
+            {
+                latitude: 37.634296,
+                longitude: 127.017533,
+                level: Constants.CROWDED_LEVEL.CROWDED
+            }
+        ],
+        map = useModel(appModel, "map"),
+        [shelterBtnList, setShelterBtnList] = useState([]);
+
+    function _onClickButton(e, markerModel) {
+        appModel.setValue("centerModel", markerModel);
+    }
+
+    function _onClickMarker(e, markerModel) {
+        appModel.markerTooltipModel.show(markerModel);
+    }
+
+    useEffect(() => {
+        if (map) {
+            const markerModels = [],
+                btnList = [];
+
+            // 대피소 마커 모델 생성
+            for (const shelter of shelterList) {
+                const {id, name, address, latitude, longitude} = shelter,
+                    markerModel = new MarkerModel({
+                        position: Util.getLocationObj({latitude, longitude}, _naverMap),
+                        title: name,
+                        desc: address,
+                        onClick: _onClickMarker,
+                        map,
+                        naverMap: _naverMap
+                    });
+
+                if (markerModel) {
+                    markerModels.push(markerModel);
+
+                    // 사이드바 내 대피소 목록 표시
+                    btnList.push(
+                        <SETextButton key={id} title={name} desc={address} onClick={e => _onClickButton(e, markerModel)}/>
+                    );
+                }
+            }
+
+            appModel.setValue("markerModels", markerModels);
+            setShelterBtnList(btnList);
+
+            // 혼잡도 모델 생성
+            for (const congestion of congestionList) {
+                const {latitude, longitude, level} = congestion,
+                    congestionModel = new CongestionModel({
+                        position: Util.getLocationObj({latitude, longitude}, _naverMap),
+                        level,
+                        map,
+                        naverMap: _naverMap
+                    });
+
+                if (congestionModel) {
+                    congestionModel.show();
+                }
+            }
+        }
+    }, [map]);
+
+    return (
+        <div className={`${_menuNames.HOME} ${_viewNames.SIDE_BAR}`}>
+            <SEImage image={`${Constants.IMAGE_URL}shelter.svg`}/>
+            <SEText className={Constants.TITLE_CLASS} desc={Resources.NEARBY_SHELTER} color={Constants.COLORS.RED}/>
+            <SEText className={Constants.SUBTITLE_CLASS} desc="강북구 삼각산로"/> {/** TODO: 테스트용 */}
+            <div className={Constants.SHELTER_LIST_CLASS}>
+                {shelterBtnList}
+            </div>
+        </div>
+    );
+};
